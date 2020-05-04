09b1ceb1b8de34f93aea37fb9066068493dae628
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-05-04 18:18:29 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,7 +5,8 @@ def find_item_by_name_in_collection(name, collection)
  
  while count < collection.length do
    name == collection[count]  #should equal a string then return obj
-   
+   final = Hash.new 
+   return final
    count += 1
  end 
   
