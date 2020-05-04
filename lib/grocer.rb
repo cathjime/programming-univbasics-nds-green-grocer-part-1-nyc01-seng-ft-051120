a4ce5b59a36d3cbc2f1b9ec30f6f5d8dd82f2d05
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-05-04 18:56:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,8 +18,9 @@ def consolidate_cart(cart)
   
   while count < cart.length do
     cart_element = cart[count]
-    binding.pry
-  # if consolidated.includes?
+    # binding.pry
+    if consolidated.include(cart_element)
+    end 
      
    count += 1
   end 
